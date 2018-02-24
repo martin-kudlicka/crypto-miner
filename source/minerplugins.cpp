60ffee8c5446f685fe588b3c501dc6e3d4a55202
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-02-24 16:21:53 +0000

    + miner plugins

---
+++ source/minerplugins.cpp
@@ -0,0 +1,5 @@
+#include "minerplugins.h"
+
+MinerPlugins::MinerPlugins() : MPluginContainer<MinerInterface>("miner")
+{
+}
---
+++ source/minerplugins.h
@@ -0,0 +1,14 @@
+#ifndef MINERPLUGINS_H
+#define MINERPLUGINS_H
+
+#include <MkCore/MPluginContainer>
+#include "../miners/common/minerinterface.h"
+
+class MinerPlugins : public MPluginContainer<MinerInterface>
+{
+  public:
+             MinerPlugins();
+    virtual ~MinerPlugins() Q_DECL_EQ_DEFAULT;
+};
+
+#endif
