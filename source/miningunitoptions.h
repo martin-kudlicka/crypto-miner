f33510d7cad0fba9770276cf84515fe67a0200cf
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-02-24 18:10:08 +0000

    + mining unit pool options

---
+++ source/miningunitoptions.h
@@ -0,0 +1,24 @@
+#ifndef MININGUNITOPTIONS_H
+#define MININGUNITOPTIONS_H
+
+#include <MkCore/MSettings>
+#include <MkCore/MUuidPtr>
+
+class MiningUnitOptions : public MSettings
+{
+  public:
+    struct Property
+    {
+      static Q_DECL_CONSTEXPR QString Pool_Address;
+      static Q_DECL_CONSTEXPR QString Pool_Password;
+      static Q_DECL_CONSTEXPR QString Pool_Wallet;
+    };
+
+             MiningUnitOptions(const MUuidPtr &id);
+    virtual ~MiningUnitOptions() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
+
+  private:
+    MUuidPtr _id;
+};
+
+#endif
