8c5bed04ce0e93c3349e9588f2f1ee65d4a1d110
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-02-24 19:32:37 +0000

    + set miner to options

--- source/miningunitoptions.h
+++ source/miningunitoptions.h
@@ -9,6 +9,7 @@ class MiningUnitOptions : public MSettings
   public:
     struct Property
     {
+      static Q_DECL_CONSTEXPR QString Miner;
       static Q_DECL_CONSTEXPR QString Pool_Address;
       static Q_DECL_CONSTEXPR QString Pool_Password;
       static Q_DECL_CONSTEXPR QString Pool_Wallet;
@@ -16,6 +17,8 @@ class MiningUnitOptions : public MSettings
 
              MiningUnitOptions(const MUuidPtr &id);
     virtual ~MiningUnitOptions() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
+
+    void setMiner(const QString &name);
 
   private:
     MUuidPtr _id;
