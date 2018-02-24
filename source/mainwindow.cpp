baa2c6fba29f19afd6deb8a4ae24f6c5e0828272
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-02-24 16:40:30 +0000

    + name in miner unit dialog

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -1,6 +1,22 @@
 #include "mainwindow.h"
 
+#include "miningunitdialog.h"
+
 MainWindow::MainWindow()
 {
   _ui.setupUi(this);
+}
+
+void MainWindow::on_miningUnitAdd_clicked(bool checked /* false */)
+{
+  // for now take first and only plugin
+  auto minerPlugin = _minerPlugins.plugins().front();
+
+  MiningUnitDialog minerDialog(*minerPlugin, this);
+  if (minerDialog.exec() == QDialog::Rejected)
+  {
+    return;
+  }
+
+  // TODO
 }
