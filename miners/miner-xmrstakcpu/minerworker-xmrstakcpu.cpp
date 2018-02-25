e81df8b2b1a77e4ca3170d9f8effa07536694be0
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-02-25 21:21:47 +0000

    + config file path

--- miners/miner-xmrstakcpu/minerworker-xmrstakcpu.cpp
+++ miners/miner-xmrstakcpu/minerworker-xmrstakcpu.cpp
@@ -1,8 +1,32 @@
 #include "minerworker-xmrstakcpu.h"
 
+#include <QtCore/QStandardPaths>
+#include <QtCore/QDir>
+#include <MkCore/MModuleInfo>
+
 MinerWorkerXmrStakCpu::MinerWorkerXmrStakCpu(const MUuidPtr &miningUnitId)
 {
   _miningUnitId = miningUnitId;
+
+  _configFilePath = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+  _configFilePath.append(QDir::separator());
+  _configFilePath.append("miners");
+  _configFilePath.append(QDir::separator());
+
+  auto moduleBaseName = MModuleInfo().fileInfo().baseName();
+  auto dashPos        = moduleBaseName.indexOf('-');
+  _configFilePath.append(moduleBaseName.mid(dashPos + 1));
+
+  QDir().mkpath(_configFilePath);
+
+  _configFilePath.append(QDir::separator());
+  _configFilePath.append(miningUnitId.toString());
+  _configFilePath.append(".txt");
+}
+
+void MinerWorkerXmrStakCpu::prepareConfigFile() const
+{
+  
 }
 
 void MinerWorkerXmrStakCpu::setPoolAddress(const QString &address)
