3ef8afe53d1338a9012aa4e8dbc3e56efec460da
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-02-25 09:36:12 +0000

    + worker pool address and credentials

--- miners/miner-xmrstakcpu/minerworker.cpp
+++ miners/miner-xmrstakcpu/minerworker.cpp
@@ -1,1 +1,11 @@
 #include "minerworker.h"
+
+void MinerWorker::setPoolAddress(const QString &address)
+{
+  _poolAddress = address;
+}
+
+void MinerWorker::setPoolCredentials(const PoolCredentials &credentials)
+{
+  _poolCredentials = credentials;
+}
