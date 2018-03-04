3baff6ee2c6cbb08b45a3341f55b134ce1b65fac
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-04 07:55:31 +0000

    * first hash rate number is enough + report hash rate to unit

--- miners/common/minerworkerinterface.h
+++ miners/common/minerworkerinterface.h
@@ -21,7 +21,8 @@ class MinerWorkerInterface : public QObject
     MUuidPtr _miningUnitId;
 
   Q_SIGNALS:
-    void resultAccepted() const;
+    void hashRate      (float value) const;
+    void resultAccepted()            const;
 };
 
 using MinerWorkerInterfaceSPtr = QSharedPointer<MinerWorkerInterface>;
