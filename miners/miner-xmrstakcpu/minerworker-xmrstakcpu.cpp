99efb4454a7c48d32c3603bb41d704fffa373654
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-04 07:55:31 +0000

    * first hash rate number is enough + report hash rate to unit

--- miners/miner-xmrstakcpu/minerworker-xmrstakcpu.cpp
+++ miners/miner-xmrstakcpu/minerworker-xmrstakcpu.cpp
@@ -159,18 +159,11 @@ void MinerWorkerXmrStakCpu::on_minerProcess_readyReadStandardOutput()
     }
     else
     {
-      regExp.setPattern(R"(^Totals:\s+(\(na\)|\d+\.\d)\s+(\(na\)|\d+\.\d)\s+(\(na\)|\d+\.\d))");
+      regExp.setPattern(R"(^Totals:\D*(\d+\.\d))");
       regExpMatch = regExp.match(_stdOutLastLine);
       if (regExpMatch.hasMatch())
       {
-        for (auto capIndex = 0; capIndex <= regExpMatch.lastCapturedIndex(); ++capIndex)
-        {
-          auto captured = regExpMatch.capturedRef(capIndex);
-          if (captured.front().isDigit())
-          {
-            auto hashRate = captured.toFloat();
-          }
-        }
+        emit hashRate(regExpMatch.capturedRef(1).toFloat());
       }
     }
 
