0fc95ab008deb6d1dda5feb649efadfbc9c94e5a
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-05 17:26:10 +0000

    + mining time

--- source/miningunitoptions.cpp
+++ source/miningunitoptions.cpp
@@ -9,6 +9,7 @@ Q_DECL_CONSTEXPR QString MiningUnitOptions::Property::Pool_Password = "pool/pass
 Q_DECL_CONSTEXPR QString MiningUnitOptions::Property::Pool_Username = "pool/username";
 
 Q_DECL_CONSTEXPR QString MiningUnitOptions::Property::Statistics_AcceptedResults = "statistics/acceptedResults";
+Q_DECL_CONSTEXPR QString MiningUnitOptions::Property::Statistics_MiningTime      = "statistics/miningTime";
 
 MiningUnitOptions::MiningUnitOptions(const MUuidPtr &id) : _id(id)
 {
@@ -29,6 +30,11 @@ const MUuidPtr &MiningUnitOptions::id() const
 QString MiningUnitOptions::miner() const
 {
   return value(Property::Miner).toString();
+}
+
+quintptr MiningUnitOptions::miningTime() const
+{
+  return value(Property::Statistics_MiningTime).toUInt();
 }
 
 QString MiningUnitOptions::poolAddress() const
@@ -54,4 +60,9 @@ void MiningUnitOptions::setAcceptedResults(quintptr count)
 void MiningUnitOptions::setMiner(const QString &name)
 {
   setValue(Property::Miner, name);
+}
+
+void MiningUnitOptions::setMiningTime(quintptr seconds)
+{
+  setValue(Property::Statistics_MiningTime, seconds);
 }
