ad8fc086bf5e2724239574cc31017f5e95597d4a
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-06 19:12:03 +0000

    + scroll at the end

--- source/consolewindow.cpp
+++ source/consolewindow.cpp
@@ -5,6 +5,8 @@ ConsoleWindow::ConsoleWindow(const QString &text)
   _ui.setupUi(this);
 
   _ui.minerText->setPlainText(text);
+
+  _ui.minerText->moveCursor(QTextCursor::End);
 }
 
 void ConsoleWindow::on_minerWorker_outputLine(const QString &line) const
