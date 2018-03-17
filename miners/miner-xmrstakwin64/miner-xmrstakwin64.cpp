8ccab5201aced12a0b5677da15ac679a742c34fc
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-17 21:50:54 +0000

    + miner interface

--- miners/miner-xmrstakwin64/miner-xmrstakwin64.cpp
+++ miners/miner-xmrstakwin64/miner-xmrstakwin64.cpp
@@ -1,5 +1,27 @@
 #include "miner-xmrstakwin64.h"
 
-MinerXmrStakWin64::MinerXmrStakWin64()
+MinerWorkerInterfaceSPtr MinerXmrStakWin64::createWorker(const MUuidPtr &id) const
 {
+  // TODO
+  return Q_NULLPTR;
+}
+
+QString MinerXmrStakWin64::name() const
+{
+  return "XMR-Stak Win64";
+}
+
+MOperatingSystemVersion::Platform MinerXmrStakWin64::platform() const
+{
+  return MOperatingSystemVersion::Platform::X64;
+}
+
+Coin::SymbolList MinerXmrStakWin64::supportedCoins() const
+{
+  return { Coin::Symbol::AEON, Coin::Symbol::XMR };
+}
+
+HwComponentList MinerXmrStakWin64::supportedHardware() const
+{
+  return { HwComponent(Company::Any, Hardware::Cpu), HwComponent(Company::Amd, Hardware::Gpu), HwComponent(Company::Nvidia, Hardware::Gpu) };
 }
