f5f62a8718b4214f117b6448dfcb60ca277ae31f
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-19 20:59:22 +0000

    * HardwareStrings renamed to HwTypeStrings

--- hardware/hwtypestrings.h
+++ hardware/hwtypestrings.h
@@ -4,11 +4,11 @@
 #include <MkCore/MStringizer>
 #include "hardware.h"
 
-class HardwareStrings : public MStringizer<Hardware::Type>
+class HwTypeStrings : public MStringizer<Hardware::Type>
 {
   public:
-             HardwareStrings();
-    virtual ~HardwareStrings() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
+             HwTypeStrings();
+    virtual ~HwTypeStrings() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
 };
 
 #endif
