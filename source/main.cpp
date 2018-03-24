01631ef6c9b08eef60072cbd9a1bec9e272c26a8
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-24 06:59:05 +0000

    + maintenance

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,16 @@ int main(int argc, char *argv[])
 
   mAnalytics->setId("UA-115441303-4");
 
+  {
+    QSettings settings;
+    auto alpha = settings.value("maintenance/alpha").toUInt();
+    if (alpha < 1)
+    {
+      settings.remove("miningUnits");
+    }
+    settings.setValue("maintenance/alpha", 1);
+  }
+
   MainWindow mainWindow;
   mainWindow.show();
 
