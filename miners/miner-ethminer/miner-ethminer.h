ac74bd6b7bb80c1146b5a11d8639ad16e45a6d4e
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-24 18:27:58 +0000

    * renamed

--- miners/miner-ethminer/miner-ethminer.h
+++ miners/miner-ethminer/miner-ethminer.h
@@ -1,9 +1,12 @@
-#pragma once
+#ifndef MINERETHMINER_H
+#define MINERETHMINER_H
 
 #include "minerethminer_global.h"
 
 class MINERETHMINER_EXPORT MinerEthMiner
 {
-public:
+  public:
     MinerEthMiner();
 };
+
+#endif
