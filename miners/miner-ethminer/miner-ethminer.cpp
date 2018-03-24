5dcd449890dee9f55a0f583217f6f48e00116630
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-24 18:55:11 +0000

    + miner interface

--- miners/miner-ethminer/miner-ethminer.cpp
+++ miners/miner-ethminer/miner-ethminer.cpp
@@ -1,5 +1,27 @@
-#include "minerethminer.h"
+#include "miner-ethminer.h"
 
-MinerEthMiner::MinerEthMiner()
+MinerWorkerInterfaceSPtr MinerEthMiner::createWorker(const MUuidPtr &id) const
 {
+  // TODO
+  return Q_NULLPTR;
+}
+
+QString MinerEthMiner::name() const
+{
+  return "ethminer";
+}
+
+MOperatingSystemVersion::Platform MinerEthMiner::platform() const
+{
+  return MOperatingSystemVersion::Platform::X64;
+}
+
+CoinList MinerEthMiner::supportedCoins() const
+{
+  return { Coin::Name::Ethereum };
+}
+
+HwComponentList MinerEthMiner::supportedHardware() const
+{
+  return { HwComponent(HwComponent::Company::Amd, HwComponent::Type::Gpu), HwComponent(HwComponent::Company::Nvidia, HwComponent::Type::Gpu) };
 }
