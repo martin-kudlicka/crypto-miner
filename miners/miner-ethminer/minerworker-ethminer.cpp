8dc5b9e3e79aa2485632b87101f3519e3a0c2a16
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2018-03-25 08:57:45 +0000

    + arguments

--- miners/miner-ethminer/minerworker-ethminer.cpp
+++ miners/miner-ethminer/minerworker-ethminer.cpp
@@ -7,6 +7,22 @@ MinerWorkerEthMiner::MinerWorkerEthMiner(const MUuidPtr &miningUnitId) : MinerWo
   _minerProcess.setProgram(_minerDir.path() + QDir::separator() + "ethminer.exe");
 }
 
+QStringList MinerWorkerEthMiner::poolArguments() const
+{
+  QStringList arguments;
+
+  arguments << "--stratum"  << _poolAddress;
+
+  auto userPass = _poolCredentials.username;
+  if (!_poolCredentials.password.isEmpty())
+  {
+    userPass += ':' + _poolCredentials.password;
+  }
+  arguments << "--userpass" << userPass;
+
+  return arguments;
+}
+
 const QLoggingCategory &MinerWorkerEthMiner::logCategory() const
 {
   return EthMiner();
@@ -19,6 +35,27 @@ void MinerWorkerEthMiner::parseStdOutLine() const
 
 QStringList MinerWorkerEthMiner::processArguments() const
 {
-  // TODO
-  return QStringList();
+  auto arguments = poolArguments();
+
+  auto hwComponent = _options.hwComponent();
+  switch (hwComponent.type())
+  {
+    case HwComponent::Type::Gpu:
+      switch (hwComponent.company())
+      {
+        case HwComponent::Company::Amd:
+          arguments << "--opencl";
+          break;
+        case HwComponent::Company::Nvidia:
+          arguments << "--cuda";
+          break;
+        default:
+          Q_ASSERT_X(false, "MinerWorkerEthMiner::processArguments", "switch (hwComponent.company())");
+      }
+      break;
+    default:
+      Q_ASSERT_X(false, "MinerWorkerEthMiner::processArguments", "switch (hwComponent.type())");
+  }
+
+  return arguments;
 }
