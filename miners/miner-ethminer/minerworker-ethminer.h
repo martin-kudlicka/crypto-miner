b540ef2c5f359caa146493eb05be975deaec44e8
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-25 08:57:45 +0000

    + arguments

--- miners/miner-ethminer/minerworker-ethminer.h
+++ miners/miner-ethminer/minerworker-ethminer.h
@@ -10,6 +10,8 @@ class MinerWorkerEthMiner : public MinerWorkerCommon
     virtual ~MinerWorkerEthMiner() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
 
   private:
+    QStringList poolArguments() const;
+
     virtual const QLoggingCategory &logCategory     () const Q_DECL_OVERRIDE;
     virtual       void              parseStdOutLine () const Q_DECL_OVERRIDE;
     virtual       QStringList       processArguments() const Q_DECL_OVERRIDE;
