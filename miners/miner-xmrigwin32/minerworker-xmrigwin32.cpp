1963af2ba124474276437638687844c136e233aa
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-30 11:22:31 +0000

    + log to console

--- miners/miner-xmrigwin32/minerworker-xmrigwin32.cpp
+++ miners/miner-xmrigwin32/minerworker-xmrigwin32.cpp
@@ -1,6 +1,7 @@
 #include "minerworker-xmrigwin32.h"
 
 #include "log.h"
+#include <QtCore/QRegularExpression>
 
 MinerWorkerXmrigWin32::MinerWorkerXmrigWin32(const MUuidPtr &miningUnitId) : MinerWorkerCommon(miningUnitId)
 {
@@ -32,7 +33,21 @@ void MinerWorkerXmrigWin32::parseStdErrLine() const
 
 void MinerWorkerXmrigWin32::parseStdOutLine() const
 {
-  // TODO
+  auto outLines = _stdOutLastLine.split(QRegularExpression(R"(\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\])"), QString::SkipEmptyParts);
+  for (auto outLine : outLines)
+  {
+    outLine      = outLine.trimmed();
+    auto message = _miningUnitId.toString() + ' ' + outLine;
+
+    if (outLine.contains("error:"))
+    {
+      mCCritical(XmrigWin32) << message;
+    }
+    else
+    {
+      mCInfo(XmrigWin32) << message;
+    }
+  }
 }
 
 QStringList MinerWorkerXmrigWin32::processArguments() const
@@ -41,8 +56,6 @@ QStringList MinerWorkerXmrigWin32::processArguments() const
 
   arguments << "--donate-level=1";
   arguments << "--max-cpu-usage=100";
-
-  // TODO
 
   return arguments;
 }
