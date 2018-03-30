e0cddf81fb7a0035b060149ce7bd13e8bf1130ee
Martin <Martin@74ca5cf1-111b-9040-b474-9a19b4a472bf>
2018-03-30 11:04:13 +0000

    + pool arguments

--- miners/miner-xmrigwin32/minerworker-xmrigwin32.cpp
+++ miners/miner-xmrigwin32/minerworker-xmrigwin32.cpp
@@ -7,6 +7,20 @@ MinerWorkerXmrigWin32::MinerWorkerXmrigWin32(const MUuidPtr &miningUnitId) : Min
   _minerProcess.setProgram(_minerDir.path() + QDir::separator() + "xmrig.exe");
 }
 
+QStringList MinerWorkerXmrigWin32::poolArguments() const
+{
+  QStringList arguments;
+
+  arguments << "--url" << _poolAddress;
+  arguments << "--user" << _poolCredentials.username;
+  if (!_poolCredentials.password.isEmpty())
+  {
+    arguments << "--pass" << _poolCredentials.password;
+  }
+
+  return arguments;
+}
+
 const QLoggingCategory &MinerWorkerXmrigWin32::logCategory() const
 {
   return XmrigWin32();
@@ -23,6 +37,9 @@ void MinerWorkerXmrigWin32::parseStdOutLine() const
 
 QStringList MinerWorkerXmrigWin32::processArguments() const
 {
+  auto arguments = poolArguments();
+
   // TODO
-  return QStringList();
+
+  return arguments;
 }
